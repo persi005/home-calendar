9ebbb5197f9b854db6aaf998eee44b31bb614b14
Vadim Serednev <vserednevdev@gmail.com>
2024-09-29 08:30:52 +0300

    added section headers & a sample of the link button

--- src/entities/adding-event/ui/AddingEventPopup/AddingEventPopup.tsx
+++ src/entities/adding-event/ui/AddingEventPopup/AddingEventPopup.tsx
@@ -2,8 +2,11 @@
 
 import styles from '@/entities/adding-event/ui/AddingEventPopup/AddingEventPopup.module.scss'
 import { ColorItem } from '@/entities/adding-event/ui/color-item/ColorItem'
+import { InputActionIcon } from '@/entities/adding-event/ui/input-action-icon/InputActionIcon'
+import { LinkButton } from '@/shared/buttons/ui/link-button/LinkButton'
 import { PopupButton } from '@/shared/buttons/ui/popup-button/PopupButton'
 import { CalendarEvent } from '@/shared/calendar-event/ui/CalendarEvent/CalendarEvent'
+import { FieldBoxHeader } from '@/shared/form/ui/field-box-header/FieldBoxHeader'
 import { Input } from '@/shared/form/ui/input/Input'
 import { Selector } from '@/shared/form/ui/selector/Selector'
 import { cn } from '@/shared/lib/utils/cn'
@@ -39,14 +42,35 @@ export function AddingEventPopup({ className }: Props) {
 
                     <hr className={styles.popup__divider} />
 
-                    <div className={styles.popup__buttonGroup}>
-                        <PopupButton variant="tinted" color="gray">
-                            Отменить
-                        </PopupButton>
-                        <PopupButton variant="tinted" color="blue">
-                            Создать
-                        </PopupButton>
+                    <div className={styles.popup__section}>
+                        <FieldBoxHeader title={'Дата события'} />
+
+                        <LinkButton>
+                            Добавить дату
+                            <InputActionIcon variant={'add'} size={'medium'} />
+                        </LinkButton>
                     </div>
+
+                    <hr className={styles.popup__divider} />
+
+                    <div className={styles.popup__section}>
+                        <FieldBoxHeader title={'Время события'} />
+                    </div>
+
+                    <hr className={styles.popup__divider} />
+
+                    <div className={styles.popup__section}>
+                        <FieldBoxHeader title={'Повторение события'} />
+                    </div>
+                </div>
+
+                <div className={styles.popup__buttonGroup}>
+                    <PopupButton variant="tinted" color="gray">
+                        Отменить
+                    </PopupButton>
+                    <PopupButton variant="tinted" color="blue">
+                        Создать
+                    </PopupButton>
                 </div>
             </div>
         </div>
