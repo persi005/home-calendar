123b8e4ef92cd164d660fe9792b8d557cccb9f61
Vadim Serednev <vserednevdev@gmail.com>
2024-10-21 17:43:48 +0300

    added `_size` attribute to the `Input` component

--- src/shared/form/ui/input/Input.tsx
+++ src/shared/form/ui/input/Input.tsx
@@ -2,10 +2,11 @@
 
 import styles from '@/shared/form/ui/input/Input.module.scss'
 import { cn } from '@/shared/lib/utils/cn'
-import { DetailedHTMLProps, InputHTMLAttributes } from 'react'
 
-type Props = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {}
+type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
+    _size?: 'small' | 'medium'
+}
 
-export function Input({ className, ...props }: Props) {
-    return <input className={cn(styles.input, className)} {...props} />
+export function Input({ _size = 'medium', className, ...props }: Props) {
+    return <input data-size={_size} className={cn(styles.input, className)} {...props} />
 }
