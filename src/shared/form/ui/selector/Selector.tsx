36ca3bdd3ee493018040b502a0d92476a09f8755
Vadim Serednev <vserednevdev@gmail.com>
2024-10-21 17:46:06 +0300

    added `size` attribute to the `Selector`

--- src/shared/form/ui/selector/Selector.tsx
+++ src/shared/form/ui/selector/Selector.tsx
@@ -8,17 +8,20 @@ import ArrowSelectorSvg from '/public/icons/symbol/arrow-select.svg'
 type Props = DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement> & {
     placeholder?: string
     value?: string
+    size?: 'small' | 'medium'
 }
 
-export function Selector({ className, value = '', placeholder = '', ...props }: Props) {
+export function Selector({ className, value = '', placeholder = '', size = 'medium', ...props }: Props) {
     const selector = useRef<HTMLDivElement>(null)
 
     useEffect(() => {
-        selector?.current?.toggleAttribute('data-placeholder', value === '')
+        if (selector.current) {
+            selector.current.toggleAttribute('data-placeholder', !value)
+        }
     }, [value])
 
     return (
-        <div ref={selector} className={cn(styles.selector, className)} {...props}>
+        <div ref={selector} className={cn(styles.selector, className)} data-size={size} {...props}>
             {value || placeholder}
             <ArrowSelectorSvg />
         </div>
