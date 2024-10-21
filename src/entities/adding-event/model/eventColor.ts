2af15fdf816284de9afd9daf44ee9cf1d254b3aa
Vadim Serednev <vserednevdev@gmail.com>
2024-10-21 17:43:00 +0300

    added `EventColor` enum

---
+++ src/entities/adding-event/model/eventColor.ts
@@ -0,0 +1,10 @@
+export enum EventColor {
+    Red = 'red',
+    Orange = 'orange',
+    Yellow = 'yellow',
+    Green = 'green',
+    Aqua = 'aqua',
+    Blue = 'blue',
+    Purple = 'purple',
+    Pink = 'pink',
+}
